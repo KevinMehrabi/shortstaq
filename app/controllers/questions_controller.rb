ec14dd26e744ca12cc214887ce110445672a21b6
Kevin Mehrabi <kmehrabi@me.com>
2015-06-05 18:26:06 -0700

    added max character count for question titles and body  using 2 methods both flash validations and text field limits

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,7 +20,8 @@ class QuestionsController < ApplicationController
       if @question.save
         redirect_to questions_path
       else
-        render :new
+         flash[:notice] = "Either your title or your question is too long. Titles must be 50 characters max, and questions must be 200 characters maximum"
+         render :new
       end
   end
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :user
+  validates :title, length: {maximum: 50}  
+  validates :body, length: {maximum: 200}
 end
